e0981426438d30be6cf0c48d3fb6daa372eee150
Farai Mrape <farai.mrape@yonder.co.za>
2023-04-22 14:51:45 +0200

    Create and bootstrap PSR-4 Linting Pipeline

---
+++ action/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run PSR-4 Linter
+vendor/bin/phpcs --standard=PSR4 ./src
+
+# Check for errors
+ERROR_COUNT=$(vendor/bin/phpcs --standard=PSR4 --report=summary ./src | grep -oP '(?<=Errors: )\d+')
+if [ $ERROR_COUNT -gt 0 ]; then
+  echo "Found $ERROR_COUNT errors in the code. Merging to the development branch is not allowed."
+  exit 1
+fi
+
+echo "PSR-4 Linting checks passed. Merging to the development branch is allowed."
