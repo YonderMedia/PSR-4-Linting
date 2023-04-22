b3044d9ee98fc677b0768f1bd495f1526babeacd
Farai Mrape <farai.mrape@yonder.co.za>
2023-04-22 14:54:16 +0200

    Update Linting Folder

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Run PSR-4 Linter
-vendor/bin/phpcs --standard=PSR4 ./src
+vendor/bin/phpcs --standard=PSR4 ./app
 
 # Check for errors
-ERROR_COUNT=$(vendor/bin/phpcs --standard=PSR4 --report=summary ./src | grep -oP '(?<=Errors: )\d+')
+ERROR_COUNT=$(vendor/bin/phpcs --standard=PSR4 --report=summary ./app | grep -oP '(?<=Errors: )\d+')
 if [ $ERROR_COUNT -gt 0 ]; then
   echo "Found $ERROR_COUNT errors in the code. Merging to the development branch is not allowed."
   exit 1
